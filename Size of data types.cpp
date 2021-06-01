a17afc49070bd0685599b50d59c0780e4f99d0f1
Nid1494 <83866450+Nid1494@users.noreply.github.com>
2021-06-01 14:01:18 +0530

    Uploaded

---
+++ Size of data types.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+int main()
+{
+	using std::cout;
+	cout<<"\n Size of char= "<<sizeof(char);
+	cout<<"\n Size of int  = "<<sizeof(int);
+	cout<<"\n Size of float = "<<sizeof(float);
+	cout<<"\n Size of double = "<<sizeof(double);
+	return 0;
+}
