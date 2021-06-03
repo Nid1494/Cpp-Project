26e4546dc02f1710f7431c103aae5734ed047446
Nid1494 <83866450+Nid1494@users.noreply.github.com>
2021-06-03 22:43:50 +0530

    Uploaded

---
+++ Number as sum of primes.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int IsPrime(int num)
+{
+	int i,prime=1;
+	for(i=2;i<=num/2;++i)
+	{
+		if(num%i==0)
+		{
+			prime=0;
+			break;
+		}
+	}
+	return prime;
+}
+int main()
+{
+	int num,i;
+	cout<<"Enter a Number: ";
+	cin>>num;
+	for(i=2;i<=num/2;++i)
+	{
+		if(IsPrime(i))
+		{
+			if(IsPrime(num-i))
+			{
+				cout<<" \n \t \t"<<num<<" = "<<i<<" + "<<num-i<<endl;
+			}
+		}
+	}
+	return 0;
+}
